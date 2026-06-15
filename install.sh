b459f15d2de4e06afb001ca4aa4ce6df1ba5cbe8
Paddy <paddy0826@163.com>
2026-06-15 21:24:34 +0800

    init

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+author=paddy
+# github=https://github.com/hzzxwl/huque-y
+
+# bash fonts colors
+red='\e[31m'
+yellow='\e[33m'
+gray='\e[90m'
+green='\e[92m'
+blue='\e[94m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+_red() { echo -e ${red}$@${none}; }
+_blue() { echo -e ${blue}$@${none}; }
+_cyan() { echo -e ${cyan}$@${none}; }
+_green() { echo -e ${green}$@${none}; }
+_yellow() { echo -e ${yellow}$@${none}; }
+_magenta() { echo -e ${magenta}$@${none}; }
+_red_bg() { echo -e "\e[41m$@${none}"; }
+
+is_err=$(_red_bg 错误!)
+is_warn=$(_red_bg 警告!)
+
+err() {
+    echo -e "\n$is_err $@\n" && exit 1
+}
+
+warn() {
+    echo -e "\n$is_warn $@\n"
+}
+
+# root
+[[ $EUID != 0 ]] && err "当前非 ${yellow}ROOT用户.${none}"
+
+
+
+
+
+
